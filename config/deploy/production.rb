401409eb087e16612ec152c15709d1fb44a433c4
uchiborieri <zuumo219@gmail.com>
2023-08-20 18:57:01 +0900

    add platform x86_64-linux

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,6 @@
-server '54.199.149.44', user: 'app', roles: %w{app db web}
+# server '54.199.149.44', user: 'app', roles: %w{app db web}
+# set :ssh_options, keys: '/Users/uchiborieri/workspace/keys/id_rsa'
+
+# TODO: app ユーザで対応する予定だが、一時的にec-user2 を利用している
+server '54.199.149.44', user: 'ec2-user', roles: %w{app db web}
 set :ssh_options, keys: '/Users/uchiborieri/workspace/keys/conkt.pem'
